9636686e49924d71bfe376ddec800ac007f8e359
Anurag Regmi <anuragregmi@protonmail.com>
2025-07-01 01:04:24 +0545

    try different way to run

--- worker.js
+++
@@ -1,8 +0,0 @@
-// worker.js
-import { createRequestHandler } from "./.next/server/handler.js";
-
-export default {
-  async fetch(request, env, ctx) {
-    return createRequestHandler({ env, ctx })(request);
-  },
-};
